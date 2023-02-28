d852eb083b009ce9f0df61b0d7aef5a02f8d600b
Jihad Oulad <114111615+jihadoulad@users.noreply.github.com>
2023-02-28 16:05:54 +0100

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,18 +16,18 @@ function Contact() {
                             
                             <form action="mailto:jihadoulad20@gmail.com" class="space-y-2">
                                 <div>
-                                    
+                                    <label for=""></label>
                                     <input type="email" id="email" class="p-6 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Name" required/>
                                 </div>
                                 <div>
-                                    
+                                    <label for=""></label>
                                     <input type="text" id="subject" class="p-6 block w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Email" required/>
                                 </div>
                                 <div class="sm:col-span-2">
-                                    
+                                    <label for=""></label>
                                     <textarea id="message" rows="6" class="block p-6 w-full text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
                                 </div>
-                                <div className=''>
+                                <div>
                                 <button type="submit" class="mt-8 py-6 px-8 font-medium text-center text-white rounded-xl bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
                                 </div>
                             </form>
