55dfd2d9087d99b87eabb1972121ba6846381774
Jihad Oulad <114111615+jihadoulad@users.noreply.github.com>
2023-02-28 01:05:01 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home"
+import Projects from "./components/Projects";
+
+
+function App() {
+  return (
+    <>
+      <Home />
+      <About/> 
+      <Projects/>
+      <Contact/>
+    </>
+    
+  );
+}
+
+export default App;
