c6296f159470ff0f73cb0dbaa05b98138a36b1c5
Jihad Oulad <114111615+jihadoulad@users.noreply.github.com>
2023-02-28 16:15:19 +0100

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,15 +16,15 @@ function Contact() {
                             
                             <form action="mailto:jihadoulad20@gmail.com" class="space-y-2">
                                 <div>
-                                    <label for=""></label>
+                                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"></label>
                                     <input type="email" id="email" class="p-6 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Name" required/>
                                 </div>
                                 <div>
-                                    <label for=""></label>
+                                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"></label>
                                     <input type="text" id="subject" class="p-6 block w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Email" required/>
                                 </div>
                                 <div class="sm:col-span-2">
-                                    <label for=""></label>
+                                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"></label>
                                     <textarea id="message" rows="6" class="block p-6 w-full text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
                                 </div>
                                 <div>
